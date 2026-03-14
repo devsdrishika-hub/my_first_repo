93dfe27be4c2001b11d1037658afaca15c2def84
Devika <devsdrish.ika@gmail.com>
2026-03-14 10:42:50 +0530

    add home page

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,9 +1,11 @@
+
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
-
+import Home from "./pages/Home";
+/*
 function App() {
   const [count, setCount] = useState(0)
 
@@ -117,5 +119,10 @@ function App() {
     </>
   )
 }
-
+*/
+return (
+  <div>
+    <Home />
+  </div>
+)
 export default App
